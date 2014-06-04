9c3b030c2e7024ee7206f41e301570dcff2f62b4
Aland Astudillo <aland.astudillo.c@gmail.com>
2014-06-04 11:55:19 -0400

    sta inspector, 2d gaussian fit, ellipse and 2d gaussian function added

---
+++ STA/sta/fmgaussfit.m
@@ -0,0 +1,133 @@
+function [fitresult, zfit, xData2D, yData2D, fiterr, zerr, resnorm, rr] = fmgaussfit(STA)
+% FMGAUSSFIT Create/alter optimization OPTIONS structure.
+%   [fitresult,..., rr] = fmgaussfit(xx,yy,zz) uses ZZ for the surface 
+%   height. XX and YY are vectors or matrices defining the x and y 
+%   components of a surface. If XX and YY are vectors, length(XX) = n and 
+%   length(YY) = m, where [m,n] = size(Z). In this case, the vertices of the
+%   surface faces are (XX(j), YY(i), ZZ(i,j)) triples. To create XX and YY 
+%   matrices for arbitrary domains, use the meshgrid function. FMGAUSSFIT
+%   uses the lsqcurvefit tool, and the OPTIMZATION TOOLBOX. The initial
+%   guess for the gaussian is places at the maxima in the ZZ plane. The fit
+%   is restricted to be in the span of XX and YY.
+%   See:
+%       http://en.wikipedia.org/wiki/Gaussian_function
+%          
+%   Examples:
+%     To fit a 2D gaussian:
+%       [fitresult, zfit, fiterr, zerr, resnorm, rr] =
+%       fmgaussfit(xx,yy,zz);
+%   See also SURF, OMPTMSET, LSQCURVEFIT, NLPARCI, NLPREDCI.
+
+%   Copyright 2013, Nathan Orloff.
+
+%% Condition the data
+fprintf('Preparing data...\n')
+[xData, yData, zData, xData2D, yData2D] = prepareSurfaceData(STA);
+xyData = {xData,yData};
+
+%% Set up the startpoint
+[amp, ind] = max(zData); % amp is the amplitude.
+xo = xData(ind); % guess that it is at the maximum
+yo = yData(ind); % guess that it is at the maximum
+ang = 45; % angle in degrees.
+sy = 1;
+sx = 1;
+zo = median(zData(:))-std(zData(:));
+xmax = max(xData);
+ymax = max(yData);
+xmin = min(xData);
+ymin = min(yData);
+
+%% Set up fittype and options.
+Lower = [0, 0, 0, 0, xmin, ymin, 0];
+Upper = [255, 180, Inf, Inf, xmax, ymax, 255]; % angles greater than 90 are redundant
+StartPoint = [amp, ang, sx, sy, xo, yo, zo];%[amp, sx, sxy, sy, xo, yo, zo];
+
+% tols = 1e-16;
+% options = optimset('Algorithm','levenberg-marquardt',...
+%     'Display','off',...
+%     'MaxFunEvals',5e2,...
+%     'MaxIter',5e2,...
+%     'TolX',tols,...
+%     'TolFun',tols,...
+%     'TolCon',tols ,...
+%     'UseParallel','always');
+
+tols = 1e-10;
+options = optimset('Algorithm','levenberg-marquardt',...
+    'Display','off',...
+    'MaxFunEvals',1e2,...
+    'MaxIter',1e2,...
+    'TolX',tols,...
+    'TolFun',tols,...
+    'TolCon',tols ,...
+    'UseParallel','always');
+
+%% perform the fitting
+fprintf('Fitting...\n')
+warning('off','optim:lsqncommon:SwitchToLargeScale')
+[fitresult,resnorm,residual] = ...
+    lsqcurvefit(@gaussian2D,StartPoint,xyData,zData,Lower,Upper,options);
+[fiterr, zfit, zerr] = gaussian2Duncert(fitresult,residual,xyData);
+rr = rsquared(zData, zfit, zerr);
+zfit = reshape(zfit,size(STA))';
+zerr = reshape(zerr,size(STA))';
+warning('on','optim:lsqncommon:SwitchToLargeScale')
+fprintf('Finished.\n\n')
+end
+
+function rr = rsquared(z,zf,ze)
+% reduced chi-squared
+dz = z-zf;
+rr = 1./(numel(z)-8).*sum(dz.^2./ze.^2); % minus 8 because there are 7 fit parameters +1 (DOF)
+end
+
+function z = gaussian2D(par,xy)
+% compute 2D gaussian
+z = par(7) + ...
+    par(1)*exp(-(((xy{1}-par(5)).*cosd(par(2))+(xy{2}-par(6)).*sind(par(2)))./par(3)).^2-...
+    ((-(xy{1}-par(5)).*sind(par(2))+(xy{2}-par(6)).*cosd(par(2)))./par(4)).^2);
+end
+
+function [dpar,zf,dzf] = gaussian2Duncert(par,resid,xy)
+% get the confidence intervals
+J = guassian2DJacobian(par,xy);
+parci = nlparci(par,resid,'Jacobian',J);
+dpar = (diff(parci,[],2)./2)';
+[zf,dzf] = nlpredci(@gaussian2D,xy,par,resid,'Jacobian',J);
+end
+
+function J = guassian2DJacobian(par,xy)
+% compute the jacobian
+x = xy{1}; y = xy{2};
+J(:,1) = exp(- (cosd(par(2)).*(x - par(5)) + sind(par(2)).*(y - par(6))).^2./par(3).^2 - (cosd(par(2)).*(y - par(6)) - sind(par(2)).*(x - par(5))).^2./par(4).^2);
+J(:,2) = -par(1).*exp(- (cosd(par(2)).*(x - par(5)) + sind(par(2)).*(y - par(6))).^2./par(3).^2 - (cosd(par(2)).*(y - par(6)) - sind(par(2)).*(x - par(5))).^2./par(4).^2).*((2.*(cosd(par(2)).*(x - par(5)) + sind(par(2)).*(y - par(6))).*(cosd(par(2)).*(y - par(6)) - sind(par(2)).*(x - par(5))))./par(3).^2 - (2.*(cosd(par(2)).*(x - par(5)) + sind(par(2)).*(y - par(6))).*(cosd(par(2)).*(y - par(6)) - sind(par(2)).*(x - par(5))))./par(4).^2);
+J(:,3) = (2.*par(1).*exp(- (cosd(par(2)).*(x - par(5)) + sind(par(2)).*(y - par(6))).^2./par(3).^2 - (cosd(par(2)).*(y - par(6)) - sind(par(2)).*(x - par(5))).^2./par(4).^2).*(cosd(par(2)).*(x - par(5)) + sind(par(2)).*(y - par(6))).^2)./par(3)^3;
+J(:,4) = (2.*par(1).*exp(- (cosd(par(2)).*(x - par(5)) + sind(par(2)).*(y - par(6))).^2./par(3).^2 - (cosd(par(2)).*(y - par(6)) - sind(par(2)).*(x - par(5))).^2./par(4).^2).*(cosd(par(2)).*(y - par(6)) - sind(par(2)).*(x - par(5))).^2)./par(4)^3;
+J(:,5) = par(1).*exp(- (cosd(par(2)).*(x - par(5)) + sind(par(2)).*(y - par(6))).^2./par(3).^2 - (cosd(par(2)).*(y - par(6)) - sind(par(2)).*(x - par(5))).^2./par(4).^2).*((2.*cosd(par(2)).*(cosd(par(2)).*(x - par(5)) + sind(par(2)).*(y - par(6))))./par(3).^2 - (2.*sind(par(2)).*(cosd(par(2)).*(y - par(6)) - sind(par(2)).*(x - par(5))))./par(4).^2);
+J(:,6) = par(1).*exp(- (cosd(par(2)).*(x - par(5)) + sind(par(2)).*(y - par(6))).^2./par(3).^2 - (cosd(par(2)).*(y - par(6)) - sind(par(2)).*(x - par(5))).^2./par(4).^2).*((2.*cosd(par(2)).*(cosd(par(2)).*(y - par(6)) - sind(par(2)).*(x - par(5))))./par(4).^2 + (2.*sind(par(2)).*(cosd(par(2)).*(x - par(5)) + sind(par(2)).*(y - par(6))))./par(3).^2);
+J(:,7) = ones(size(x));
+end
+
+function [Xpixels, Ypixels, valores, Xpixel2D, Ypixel2D] = prepareSurfaceData(matrizSTA)
+% prepare surface data using meshgrid
+matrizSTA = double(matrizSTA);
+vec1 = [];
+vec2 = [];
+vec3 = [];
+[n_filas,n_columnas] = size(matrizSTA);
+[Xpixel2D,Ypixel2D] = meshgrid(1:n_columnas,1:n_filas);
+for y=1:n_filas,
+    for x=1:n_columnas,
+        Xpixels = [vec1 Xpixel2D(y,x)];
+        Ypixels = [vec2 Ypixel2D(y,x)];
+        valores = [vec3 matrizSTA(y,x)];
+        vec1 = Xpixels;
+        vec2 = Ypixels;
+        vec3 = valores;
+    end
+end
+Xpixels = Xpixels';
+Ypixels = Ypixels';
+valores = valores';
+end
---
+++ STA/sta/sta_inspector.m
@@ -0,0 +1,484 @@
+%% sta inspector
+% AASTUDILLO2014
+% 20 mayo 2014
+
+% path = '10umDRUG_BGA'; % list_dir = dir(fullfile(path)); % list_dir = {list_dir.name}';
+% UNITS ={'C10b','C12a','D3a','D3b','D12b','D15b','D15c','E14a','F15c','G11a'};
+
+datos = '0001';
+
+units = textread(['units_',datos,'.txt'],'%s','delimiter','\t');
+
+%charac = load(['characterization_',datos,'.txt']);
+charac = ones(length(units),1);
+
+snr_inspector = zeros(length(units),1);
+
+stafolder = ['STA_datos',datos,'_fast2'];
+
+classfolder = 'sta_inspector';
+mkdir([stafolder,'/',classfolder]);
+
+dimensiones = 5;
+nclusters = 6;
+
+inicio = 1;
+limite = 429; %23+5; %length(charac); 
+
+doplot = 0;
+numframes = 20;
+spikeframe = 18;
+contadorunit = 1;
+
+dotemp = 0;
+
+for kunit = inicio:limite
+if (charac(kunit)>0)
+    indexvector(contadorunit) = kunit;
+%%
+% FOLDER NAME OF THE CELL
+
+charunit = char(units(kunit));
+carpeta = [stafolder,'/',charunit,'_lineal/'];
+
+nombre_cell_grupo = char(charunit);
+
+load([carpeta,'stavisual_lin_array_',nombre_cell_grupo,'.mat']);
+load([carpeta,'sta_array_',nombre_cell_grupo,'.mat']);
+% STAarray_lin(:,:,:) = stavisual_lin(:,:,3,:);
+% STA_array(:,:,:) = STA(:,:,3,:);
+% STA_array(:,:,:) = STA(:,:,3,:);
+
+% clear stavisual_lin
+% clear STA
+
+img_total=STAarray_lin(:);
+ima_total=max(img_total(:));
+imi_total=min(img_total(:));
+ims_total=std(img_total(:));
+snr_total=20*log10((ima_total-imi_total)./ims_total);
+
+snr_inspector(kunit) = snr_total;
+% snr = 10.147 % which fulfills the rose criterion...
+
+%%
+%--------------------------------------------------------------------------
+if doplot
+    fig1=figure('Visible','off');
+    for k=1:18; 
+        subplot(3,6,k); 
+        imshow(uint8(STAarray_lin(:,:,k))); 
+        
+%         img=STAarray_lin(:,:,k);
+%         ima=max(img(:));
+%         imi=min(img(:));
+%         ims=std(img(:));
+%         snr=20*log10((ima-imi)./ims);
+%         title(['SNR ',num2str(snr)]);
+    end; 
+    title(['SNR total ',num2str(snr_total)]);
+    colormap('jet');
+    %save([carpeta,'time_curves_',nombre_cell_grupo,'.mat'],'temporalcurve1','tvector','spline_curve','frame_ajuste','fc');
+    print(fig1,'-dpng',[stafolder,'/',classfolder,'/',charunit,'.png']);
+end
+%--------------------------------------------------------------------------
+
+if dotemp
+ %% normalize STA matrix for plot purposes
+STA_promedio = mean(STAarray_lin,3);
+
+STA = STAarray_lin(:,end:-1:1,:);
+
+[n_filas,n_columnas] = size(STA_promedio);
+
+promedio = mean(STA_promedio(:));
+
+[min_sta,frame_min] = min(min(min(STA)));
+[max_sta,frame_max] = max(max(max(STA)));
+amp_min = promedio - min_sta;
+amp_max = max_sta - promedio;
+
+if amp_min >= amp_max
+    frame_ajuste = frame_min;
+%     STA_ajuste = (STA(:,:,frame_ajuste) - 255)*(-1); 
+    ss = STAarray_lin(:,:,frame_ajuste);
+    [fila,col]=find(ss==min(min(ss)));
+else
+    frame_ajuste = frame_max;
+%     STA_ajuste = STA(:,:,frame_ajuste);
+    ss = STAarray_lin(:,:,frame_ajuste);
+    [fila,col]=find(ss==max(max(ss)));
+end
+
+% fc = [fila(1),col(1)];
+fc = [round(mean(fila)),round(mean(col))];
+
+% temporalcurve = STAarray_lin(fc(1),fc(2),:);
+% tempcurve = temporalcurve(:);
+
+temporalcurve1 = STA_array(fc(1),fc(2),:);
+temporalcurve2 = STAarray_lin(fc(1),fc(2),:);
+tempcurve = temporalcurve1(:)/sum(abs(temporalcurve1));
+
+tvector = ((1:numframes)-spikeframe)/60*1000;
+
+pp = csaps(tvector,tempcurve);
+spline_curve = fnplt(pp);
+% spline_curve = points;
+
+alltempcurves(:,contadorunit) = tempcurve;
+alltempcurvesinterpolated(:,:,contadorunit) = spline_curve;
+contadorunit = contadorunit+1;
+
+save([carpeta,'time_curves_',nombre_cell_grupo,'.mat'],'temporalcurve1','tvector','spline_curve','frame_ajuste','fc');
+
+%%
+% if doplot
+%     figure;
+%     imshow(uint8(sta(:,:,frame_ajuste))); 
+%     colormap('gray'); 
+%     colorbar;
+%     hold on, 
+%     line([0 400],[fc(2) fc(2)],'Color','r')
+%     line([fc(1) fc(1)],[0 400],'Color','r')
+% end
+
+%%
+% if doplot
+%     tvector = ((1:18)-13)/60*1000;
+%     figure, 
+%     plot(tvector,tempcurve,'LineWidth',2);
+%     hold on
+%     line([0 0],[0 255],'Color','r','LineWidth',2);
+%     grid
+%     xlabel('Time ms');
+%     ylabel('Amplitude');
+%     xlim([-200 50]);
+%     ylim([-2 256]);
+% end
+end
+disp(['k unit ',num2str(kunit)])
+end
+end
+
+%%
+% normalize each curve
+if dotemp
+donorm = 0;
+if donorm
+for k=1:size(alltempcurves,2) %size(list_dir,1)
+    alltempcurves2(:,k) = alltempcurves(:,k) - alltempcurves(spikeframe,k);
+end
+else 
+    alltempcurves2 = alltempcurves;
+end
+end
+
+%%
+if dotemp
+tvector = ((1:numframes)-spikeframe)/60*1000;
+h1=figure;
+plot(tvector,alltempcurves2,'Color','b','LineWidth',1);
+hold on
+line([0 0],[-100 50],'Color','r','LineWidth',2);
+grid
+xlabel('Time ms');
+ylabel('Amplitude');
+title('Temporal Profiles');
+xlim([-300 40]);
+ylim([-0.35 0.35]);
+
+%%
+h1_1=figure;
+xcurve(1:size(alltempcurvesinterpolated,2),1:size(alltempcurvesinterpolated,3)) = alltempcurvesinterpolated(1,:,:);
+ycurve(1:size(alltempcurvesinterpolated,2),1:size(alltempcurvesinterpolated,3)) = alltempcurvesinterpolated(2,:,:);
+
+plot(xcurve,ycurve,'Color','b','LineWidth',1);
+hold on
+line([0 0],[-100 50],'Color','r','LineWidth',2);
+grid
+xlabel('Time ms');
+ylabel('Amplitude');
+title('Temporal Profiles');
+xlim([-300 40]);
+ylim([-0.35 0.35]);
+
+%%
+X = alltempcurves2';
+[COEFF,SCORE,latent] = princomp(X);
+
+%%
+h2=figure;
+plot3(SCORE(:,1),SCORE(:,2),SCORE(:,3),'b.');
+grid;
+xlabel('PC1');
+ylabel('PC2');
+zlabel('PC3');
+title('Feature space PCA');
+
+%%
+opts = statset('Display','final');
+
+X2 = SCORE(:,1:dimensiones);
+[idx,ctrs] = kmeans(X2,nclusters,'Distance','sqEuclidean','Replicates',10,'Options',opts,'start','cluster');
+
+%%
+h3=figure;
+plot(X2(idx==1,1),X2(idx==1,2),'r.','MarkerSize',12)
+hold on
+plot(X2(idx==2,1),X2(idx==2,2),'b.','MarkerSize',12)
+plot(X2(idx==3,1),X2(idx==3,2),'g.','MarkerSize',12)
+plot(X2(idx==4,1),X2(idx==4,2),'m.','MarkerSize',12)
+plot(X2(idx==5,1),X2(idx==5,2),'c.','MarkerSize',12)
+plot(X2(idx==6,1),X2(idx==6,2),'y.','MarkerSize',12)
+plot(ctrs(:,1),ctrs(:,2),'kx','MarkerSize',12,'LineWidth',2)
+plot(ctrs(:,1),ctrs(:,2),'ko','MarkerSize',12,'LineWidth',2)
+grid
+xlabel('PC1');
+ylabel('PC2');
+title('Features space PCA');
+
+%%
+h3_3=figure;
+plot3(SCORE(idx==1,1),SCORE(idx==1,2),SCORE(idx==1,3),'r.'); hold on
+plot3(SCORE(idx==2,1),SCORE(idx==2,2),SCORE(idx==2,3),'b.');
+plot3(SCORE(idx==3,1),SCORE(idx==3,2),SCORE(idx==3,3),'g.');
+plot3(SCORE(idx==4,1),SCORE(idx==4,2),SCORE(idx==4,3),'m.');
+plot3(SCORE(idx==5,1),SCORE(idx==5,2),SCORE(idx==5,3),'c.');
+plot3(SCORE(idx==6,1),SCORE(idx==6,2),SCORE(idx==6,3),'y.');
+grid;
+xlabel('PC1');
+ylabel('PC2');
+zlabel('PC3');
+title('Feature space PCA');
+
+%%
+meancurve1=mean(alltempcurves2(:,idx==1),2);
+meancurve2=mean(alltempcurves2(:,idx==2),2);
+meancurve3=mean(alltempcurves2(:,idx==3),2);
+meancurve4=mean(alltempcurves2(:,idx==4),2);
+meancurve5=mean(alltempcurves2(:,idx==5),2);
+meancurve6=mean(alltempcurves2(:,idx==6),2);
+
+tvector = ((1:numframes)-spikeframe)/60*1000;
+
+h4=figure;
+plot(tvector,alltempcurves2(:,idx==1),'Color','r','LineWidth',1); hold on
+plot(tvector,alltempcurves2(:,idx==2),'Color','b','LineWidth',1);
+plot(tvector,alltempcurves2(:,idx==3),'Color','g','LineWidth',1);
+plot(tvector,alltempcurves2(:,idx==4),'Color','m','LineWidth',1);
+plot(tvector,alltempcurves2(:,idx==5),'Color','c','LineWidth',1);
+plot(tvector,alltempcurves2(:,idx==6),'Color','y','LineWidth',1);
+plot(tvector,meancurve1,'k--','LineWidth',3);
+plot(tvector,meancurve2,'k--','LineWidth',3);
+plot(tvector,meancurve3,'k--','LineWidth',3);
+plot(tvector,meancurve4,'k--','LineWidth',3);
+plot(tvector,meancurve5,'k--','LineWidth',3);
+plot(tvector,meancurve6,'k--','LineWidth',3);
+line([0 0],[-255 255],'Color','r','LineWidth',2);
+grid
+xlabel('Time ms');
+ylabel('Amplitude');
+title('Temporal Profile');
+xlim([-300 40]);
+ylim([-0.35 0.35]);
+
+
+
+%% subplots of each cluster
+% tvector = ((1:numframes)-spikeframe)/60*1000;
+
+h5=figure;
+subplot(2,3,1); 
+plot(tvector,alltempcurves2(:,idx==1),'Color','r','LineWidth',1); hold on
+plot(tvector,meancurve1,'k--','LineWidth',3);
+line([0 0],[-255 255],'Color','r','LineWidth',2);
+grid; xlabel('Time ms'); ylabel('Amplitude'); title('Temporal Profile Cluster1');
+xlim([-300 40]);
+ylim([-0.35 0.35]);
+
+subplot(2,3,2); 
+plot(tvector,alltempcurves2(:,idx==2),'Color','b','LineWidth',1); hold on
+plot(tvector,meancurve2,'k--','LineWidth',3);
+line([0 0],[-255 255],'Color','r','LineWidth',2);
+grid; xlabel('Time ms'); ylabel('Amplitude'); title('Temporal Profile Cluster2');
+xlim([-300 40]);
+ylim([-0.35 0.35]);
+
+subplot(2,3,3); 
+plot(tvector,alltempcurves2(:,idx==3),'Color','g','LineWidth',1); hold on
+plot(tvector,meancurve3,'k--','LineWidth',3);
+line([0 0],[-255 255],'Color','r','LineWidth',2);
+grid; xlabel('Time ms'); ylabel('Amplitude'); title('Temporal Profile Cluster3');
+xlim([-300 40]);
+ylim([-0.35 0.35]);
+
+subplot(2,3,4); 
+plot(tvector,alltempcurves2(:,idx==4),'Color','m','LineWidth',1); hold on
+plot(tvector,meancurve4,'k--','LineWidth',3);
+line([0 0],[-255 255],'Color','r','LineWidth',2);
+grid; xlabel('Time ms'); ylabel('Amplitude'); title('Temporal Profile Cluster4');
+xlim([-300 40]);
+ylim([-0.35 0.35]);
+
+subplot(2,3,5); 
+plot(tvector,alltempcurves2(:,idx==5),'Color','c','LineWidth',1); hold on
+plot(tvector,meancurve5,'k--','LineWidth',3);
+line([0 0],[-255 255],'Color','r','LineWidth',2);
+grid; xlabel('Time ms'); ylabel('Amplitude'); title('Temporal Profile Cluster5');
+xlim([-300 40]);
+ylim([-0.35 0.35]);
+
+subplot(2,3,6); 
+plot(tvector,alltempcurves2(:,idx==6),'Color','y','LineWidth',1); hold on
+plot(tvector,meancurve6,'k--','LineWidth',3);
+line([0 0],[-255 255],'Color','r','LineWidth',2);
+grid; xlabel('Time ms'); ylabel('Amplitude'); title('Temporal Profile Cluster5');
+xlim([-300 40]);
+ylim([-0.35 0.35]);
+
+%%
+h4_1=figure;
+%plot(xcurve,ycurve,'Color','b','LineWidth',1);
+
+mc1=mean(ycurve(:,idx==1),2);
+mc2=mean(ycurve(:,idx==2),2);
+mc3=mean(ycurve(:,idx==3),2);
+mc4=mean(ycurve(:,idx==4),2);
+mc5=mean(ycurve(:,idx==5),2);
+mc6=mean(ycurve(:,idx==6),2);
+
+plot(xcurve(:,1),ycurve(:,idx==1),'Color','r','LineWidth',1); hold on
+plot(xcurve(:,1),ycurve(:,idx==2),'Color','b','LineWidth',1);
+plot(xcurve(:,1),ycurve(:,idx==3),'Color','g','LineWidth',1);
+plot(xcurve(:,1),ycurve(:,idx==4),'Color','m','LineWidth',1);
+plot(xcurve(:,1),ycurve(:,idx==5),'Color','c','LineWidth',1);
+plot(xcurve(:,1),ycurve(:,idx==6),'Color','y','LineWidth',1);
+plot(xcurve(:,1),mc1,'k--','LineWidth',3);
+plot(xcurve(:,1),mc2,'k--','LineWidth',3);
+plot(xcurve(:,1),mc3,'k--','LineWidth',3);
+plot(xcurve(:,1),mc4,'k--','LineWidth',3);
+plot(xcurve(:,1),mc5,'k--','LineWidth',3);
+plot(xcurve(:,1),mc6,'k--','LineWidth',3);
+line([0 0],[-255 255],'Color','r','LineWidth',2);
+grid
+xlabel('Time ms');
+ylabel('Amplitude');
+title('Temporal Profile');
+xlim([-300 40]);
+ylim([-0.35 0.35]);
+
+%%
+%tvector = ((1:numframes)-spikeframe)/60*1000;
+%plot(xcurve,ycurve,'Color','b','LineWidth',1);
+h5_1=figure;
+
+subplot(2,3,1); 
+plot(xcurve(:,1),ycurve(:,idx==1),'Color','r','LineWidth',1); hold on
+plot(xcurve(:,1),mc1,'k--','LineWidth',3);
+line([0 0],[-255 255],'Color','r','LineWidth',2);
+grid; xlabel('Time ms'); ylabel('Amplitude'); title('Temporal Profile Cluster1');
+xlim([-300 40]);
+ylim([-0.35 0.35]);
+
+subplot(2,3,2); 
+plot(xcurve(:,1),ycurve(:,idx==2),'Color','b','LineWidth',1); hold on
+plot(xcurve(:,1),mc2,'k--','LineWidth',3);
+line([0 0],[-255 255],'Color','r','LineWidth',2);
+grid; xlabel('Time ms'); ylabel('Amplitude'); title('Temporal Profile Cluster2');
+xlim([-300 40]);
+ylim([-0.35 0.35]);
+
+subplot(2,3,3); 
+plot(xcurve(:,1),ycurve(:,idx==3),'Color','g','LineWidth',1); hold on
+plot(xcurve(:,1),mc3,'k--','LineWidth',3);
+line([0 0],[-255 255],'Color','r','LineWidth',2);
+grid; xlabel('Time ms'); ylabel('Amplitude'); title('Temporal Profile Cluster3');
+xlim([-300 40]);
+ylim([-0.35 0.35]);
+
+subplot(2,3,4); 
+plot(xcurve(:,1),ycurve(:,idx==4),'Color','m','LineWidth',1); hold on
+plot(xcurve(:,1),mc4,'k--','LineWidth',3);
+line([0 0],[-255 255],'Color','r','LineWidth',2);
+grid; xlabel('Time ms'); ylabel('Amplitude'); title('Temporal Profile Cluster4');
+xlim([-300 40]);
+ylim([-0.35 0.35]);
+
+subplot(2,3,5); 
+plot(xcurve(:,1),ycurve(:,idx==5),'Color','c','LineWidth',1); hold on
+plot(xcurve(:,1),mc5,'k--','LineWidth',3);
+line([0 0],[-255 255],'Color','r','LineWidth',2);
+grid; xlabel('Time ms'); ylabel('Amplitude'); title('Temporal Profile Cluster5');
+xlim([-300 40]);
+ylim([-0.35 0.35]);
+
+subplot(2,3,6); 
+plot(xcurve(:,1),ycurve(:,idx==6),'Color','y','LineWidth',1); hold on
+plot(xcurve(:,1),mc6,'k--','LineWidth',3);
+line([0 0],[-255 255],'Color','r','LineWidth',2);
+grid; xlabel('Time ms'); ylabel('Amplitude'); title('Temporal Profile Cluster5');
+xlim([-300 40]);
+ylim([-0.35 0.35]);
+
+%% save images
+
+
+print(h1,'-dpdf',[stafolder,'/',classfolder,'/h1_temp_prof.pdf']);
+print(h2,'-dpdf',[stafolder,'/',classfolder,'/h2_pca.pdf']);
+print(h3,'-dpdf',[stafolder,'/',classfolder,'/h3_pca_kmeans.pdf']);
+print(h3_3,'-dpdf',[stafolder,'/',classfolder,'/h3_3_pca_kmeans.pdf']);
+print(h4,'-dpdf',[stafolder,'/',classfolder,'/h4_temp_clustered.pdf']);
+print(h5,'-dpdf',[stafolder,'/',classfolder,'/h5_tp_clusters.pdf']);
+
+print(h1_1,'-dpdf',[stafolder,'/',classfolder,'/h1_spline_temp_prof.pdf']);
+print(h4_1,'-dpdf',[stafolder,'/',classfolder,'/h4_spline_temp_clustered.pdf']);
+print(h5_1,'-dpdf',[stafolder,'/',classfolder,'/h5_spline_tp_clusters.pdf']);
+
+% PNGS
+print(h1,'-dpng',[stafolder,'/',classfolder,'/h1_temp_prof.png']);
+print(h2,'-dpng',[stafolder,'/',classfolder,'/h2_pca.png']);
+print(h3,'-dpng',[stafolder,'/',classfolder,'/h3_pca_kmeans.png']);
+print(h3_3,'-dpng',[stafolder,'/',classfolder,'/h3_3_pca_kmeans.png']);
+print(h4,'-dpng',[stafolder,'/',classfolder,'/h4_temp_clustered.png']);
+print(h5,'-dpng',[stafolder,'/',classfolder,'/h5_tp_clusters.png']);
+
+print(h1_1,'-dpng',[stafolder,'/',classfolder,'/h1_spline_temp_prof.png']);
+print(h4_1,'-dpng',[stafolder,'/',classfolder,'/h4_spline_temp_clustered.png']);
+print(h5_1,'-dpng',[stafolder,'/',classfolder,'/h5_spline_tp_clusters.png']);
+
+%% get the labels of each unit
+% units_line = list_dir(3:end);
+% units = {};
+% for k =1:size(units_line,1)
+%     u = char(units_line(k));
+%     units{k} = [u(1:end-7)];
+% end
+% 
+% table_id = [units(1:limite)];
+% 
+% for k=1:size(table_id,2)
+%     table_id{2,k} = (idx(k));
+% end
+% 
+% save([path,'_table_id.mat'],'table_id');
+cont =1;
+for k=indexvector;
+    table_id{1,k}=units(k);
+    table_id{2,k}=charac(k);
+    table_id{3,k}=idx(cont);
+    
+    table_id_mini{1,cont}=units(k);
+    table_id_mini{2,cont}=charac(k);
+    table_id_mini{3,cont}=idx(cont);
+    cont=cont+1;
+end
+save([stafolder,'/',classfolder,'/classification.mat'],'table_id','table_id_mini');
+%% IMPROVE THE TEMPORAL PROFILE PLOTS:
+
+save([stafolder,'/',classfolder,'/all_temp_curves.mat'],'units','charac','idx','tvector','alltempcurves','alltempcurvesinterpolated','SCORE','COEFF','xcurve','ycurve');
+
+end
+
+% snr_inspector
+save([stafolder,'/',classfolder,'/snr_inspector.mat'],'snr_inspector');
