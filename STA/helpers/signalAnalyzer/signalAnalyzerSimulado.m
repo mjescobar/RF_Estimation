d4e195ef2de59149876d6534d234796887fb988b
monicaot2001 <monicaot2001@gmail.com>
2014-10-02 12:26:36 -0300

    Generacion del inicio_fin frames a partir de punto inicial
    Generacion del inicio_fin frames a partir de punto inicial

--- STA/helpers/signalAnalyzer/signalAnalyzerSimulado.m
+++ STA/helpers/signalAnalyzer/signalAnalyzerSimulado.m
@@ -14,6 +14,7 @@ end
 % % 
 [nsresult, hfile] = ns_OpenFile([mcdFile])
 [nsresult,info_file] = ns_GetFileInfo(hfile)
+disp(info_file);
 
 [nsresult,entity] = ns_GetEntityInfo(hfile,entityNumber)
 % % 
@@ -24,7 +25,7 @@ umbral_volts = 0.151111112; %0.1511 ; % umbral en volts, determina criticamente 
 puntos_pulso = 50; %24*2; 24; % puntos maximos requeridos para considerar como un pulso de frame
 
 duracion_min = length(data)/sampleRating/60; % duracion en minutos de la senal
-
+disp (duracion_min)
 for k = puntos_pulso+1:length(data)-2
     if data(k) > umbral_volts
         if (data(k-1)>umbral_volts) && (data(k-puntos_pulso)< umbral_volts) && (data(k+1)<umbral_volts) && (data(k+1)<umbral_volts)
@@ -33,5 +34,27 @@ for k = puntos_pulso+1:length(data)-2
         end
     end
 end
+
+valorFinal= tiempoTotal*sampleRating*60;
+iterations= floor(valorFinal/334); 
+inicioFrames= zeros(iterations,2);
+puntoActual=primerPunto;
+
+
+for j = 1:iterations
+    inicioFrames(j,1)=puntoActual;
+    if (mod(j,2)==0)
+        puntoActual=puntoActual+334;
+        inicioFrames(j,2)=puntoActual;
+    else 
+        puntoActual=puntoActual+335;
+        inicioFrames(j,2)=puntoActual;
+    end
+end
+
+save([outputFolder,'Inicio_Frames',experimentName,'.txt' ],'inicioFrames', '-ASCII' ,'-DOUBLE');
+    
+
+
 
 disp (primerPunto)
